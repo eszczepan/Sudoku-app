c0deaecca02d13aebcf80dbbd2aecd0255385620
Szczepan <s.szablej1@gmail.com>
2021-03-05 19:04:20 +0100

    Create copyGrid function and write test for this

---
+++ src/utils/copy-grid/index.ts
@@ -0,0 +1,25 @@
+import { GRID, INDEX } from 'typings'
+
+function copyGrid(grid: GRID): GRID {
+  const gridCopy: GRID = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]
+
+  for (let i: INDEX = 0; i < 9; i++) {
+    for (let j: INDEX = 0; j < 9; j++) {
+      gridCopy[i][j] = grid[i][j]
+    }
+  }
+
+  return gridCopy
+}
+
+export default copyGrid
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export { default as fillGrid } from './fill-grid'
+export { default as copyGrid } from './copy-grid'
 export { default as getRandomIndex } from './get-random-index'
 export { default as createFullGrid } from './create-full-grid'
 export { default as shuffle } from './shuffle'
